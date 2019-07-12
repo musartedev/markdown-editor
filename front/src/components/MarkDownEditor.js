a20301346ef8c321b6ca99607fbe0c12c6e36d2f
Mariangelica Useche <mariangelica.useche@gmail.com>
2019-07-12 14:25:38 -0400

    [Frontend] - MarkdownEditor component added.

---
+++ front/src/components/MarkDownEditor.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Row, Col, Input } from "antd";
+import showdown from "showdown";
+
+export default function MarkDownEditor(props) {
+  const { rawText, handleOnChange } = props;
+
+  const renderResult = () => {
+    const converter = new showdown.Converter();
+    return { __html: converter.makeHtml(rawText) };
+  };
+
+  return (
+    <Row>
+      <Col
+        className="gutter-row"
+        style={{ backgroundColor: "black" }}
+        span={12}
+      >
+        <Input.TextArea
+          className="textarea"
+          value={rawText}
+          onChange={({ target }) => handleOnChange(target.value)}
+        />
+      </Col>
+      <Col className="gutter-row markdown-container" span={12}>
+        <div
+          className="markdown-body"
+          dangerouslySetInnerHTML={renderResult()}
+        />
+      </Col>
+    </Row>
+  );
+}
